073c7933039630b4fe28770c9c1b0ba4ebba652a
Винер <viner.ahatovi4.ru@gmail.com>
2026-07-15 12:50:19 +0300

    fix: correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/REACT-3.2.3-AKHATOV-VINER/',
    test: {
     globals: true,
     environment: 'jsdom',
